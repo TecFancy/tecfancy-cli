2ab3e48422cad162253cd5eace0ec25127bd3534
Randal Wang <randal_wang@resourcepro.com.cn>
2024-01-04 18:38:39 +0800

    Refactor init function to create directory if it doesn't exist

--- core/cli/src/init.ts
+++ core/cli/src/init.ts
@@ -1,13 +1,21 @@
-'use strict';
+"use strict";
 
+import path from "path";
+import fse from "fs-extra";
 import log from "@tecfancy/log";
 
 interface OptionsType {
   force?: boolean;
 }
 
-function init(name: string, options: OptionsType) {
-  log.info('init', name, options);
+async function init(name: string, options: OptionsType) {
+  const dirPath = path.join(process.cwd(), name);
+  if (fse.existsSync(dirPath)) {
+    log.warn('', 'The directory already exists, continue to overwrite?');
+  } else {
+    fse.mkdirSync(dirPath);
+    log.success('', 'The directory has been created successfully!');
+  }
 }
 
 export default init;
