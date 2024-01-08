0b33a0f6566cef32c0dd81d56bfe1fb191c7d03c
smpower <rf.wangchn@foxmail.com>
2024-01-08 21:34:24 +0800

    Add CACHE_DIR and NODE_MODULES_DIR constants

--- core/cli/src/const.ts
+++ core/cli/src/const.ts
@@ -7,4 +7,9 @@ import { homedir as userHome } from "os";
 export const DEFAULT_CLI_HOME = path.join(userHome(), ".config", "tecfancy");
 
 // environment configuration file path
-export const DOTENV_PATH = path.resolve(DEFAULT_CLI_HOME, ".env");
+export const DOTENV_PATH = path.join(DEFAULT_CLI_HOME, ".env");
+
+// cache directory path
+export const CACHE_DIR = path.join(DEFAULT_CLI_HOME, "cache");
+
+export const NODE_MODULES_DIR = path.join(DEFAULT_CLI_HOME, "node_modules");
--- core/cli/src/init.ts
+++ core/cli/src/init.ts
@@ -1,12 +1,24 @@
 "use strict";
 
 import path from "path";
+import fse from "fs-extra";
 import log from "@tecfancy/log";
-import createDir from './createDir.js';
+import createDir from "./createDir.js";
+import { CACHE_DIR, NODE_MODULES_DIR } from "./const.js";
 
 interface OptionsType {
   force?: boolean;
 }
+
+function createNodeModulesDir() {
+  if (!fse.pathExistsSync(NODE_MODULES_DIR)) {
+    fse.mkdirpSync(NODE_MODULES_DIR);
+  }
+}
+
+function getProjectsList() {}
+
+function cloneRepo() {}
 
 /**
  * Initialize the project
@@ -19,6 +31,9 @@ async function init(projectName: string | undefined, options: OptionsType) {
       ? path.join(process.cwd(), projectName)
       : process.cwd();
     await createDir(dirPath, options.force);
+    createNodeModulesDir();
+    getProjectsList();
+    cloneRepo();
   } catch (error) {
     log.error("", `Initialization failed: ${error}`);
     throw new Error(`Initialization failed: ${error}`); // Propagate the exception to be handled by the caller
