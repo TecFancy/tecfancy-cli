381b7596d66c040df2438e4cc8d6b00556861521
smpower <rf.wangchn@foxmail.com>
2024-01-12 20:20:57 +0800

    Update configuration constants in index.ts

--- utils/const/src/index.ts
+++ utils/const/src/index.ts
@@ -6,34 +6,37 @@ import { homedir as userHome } from "os";
 import getEnvParam from "./getEnvParam";
 
 // configure the directory path of the configuration file
-export const DEFAULT_CLI_HOME = getEnvParam(
-  "TECFANCY_DEFAULT_CLI_HOME",
+export const TECFANCY_CLI_HOME = getEnvParam(
+  "TECFANCY_CLI_HOME",
   path.join(userHome(), ".config", "tecfancy")
 );
 
 // environment configuration file path
-export const DOTENV_PATH = getEnvParam(
-  DEFAULT_CLI_HOME,
-  path.join(DEFAULT_CLI_HOME, ".env")
+export const TECFANCY_CLI_DOTENV_PATH = getEnvParam(
+  TECFANCY_CLI_HOME,
+  path.join(TECFANCY_CLI_HOME, ".env")
 );
 
 // cache directory path
-export const CACHE_DIR = getEnvParam(
-  DEFAULT_CLI_HOME,
-  path.join(DEFAULT_CLI_HOME, "cache")
+export const TECFANCY_CLI_CACHE_DIR = getEnvParam(
+  TECFANCY_CLI_HOME,
+  path.join(TECFANCY_CLI_HOME, "cache")
 );
 
 // node_modules directory path
-export const NODE_MODULES_DIR = getEnvParam(
-  CACHE_DIR,
-  path.join(CACHE_DIR, "node_modules")
+export const TECFANCY_CLI_NODE_MODULES_DIR = getEnvParam(
+  TECFANCY_CLI_CACHE_DIR,
+  path.join(TECFANCY_CLI_CACHE_DIR, "node_modules")
 );
 
 // store directory path
-export const STORE_DIR = getEnvParam(CACHE_DIR, path.join(CACHE_DIR, "store"));
+export const STORE_DIR = getEnvParam(
+  TECFANCY_CLI_CACHE_DIR,
+  path.join(TECFANCY_CLI_CACHE_DIR, "store")
+);
 
 // registry url
 export const REGISTRY = getEnvParam(
-  "TECFANCY_CLI_REGISTRY",
+  "TECFANCY_CLI_REGISTRY_URL",
   "https://registry.npmjs.org"
 );
