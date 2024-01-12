d030656f1885e7c15e702b1031a3a13801d0b636
smpower <rf.wangchn@foxmail.com>
2024-01-12 22:03:14 +0800

    Update setupEnvConfig to update or create environment variables

--- core/cli/src/setupEnvConfig.ts
+++ core/cli/src/setupEnvConfig.ts
@@ -1,11 +1,51 @@
 "use strict";
 
 import fse from "fs-extra";
-import { DEFAULT_CLI_HOME, DOTENV_PATH } from "@tecfancy/const";
+import * as envVariables from "@tecfancy/const";
+
+function updateOrCreateEnvVariables(envVariables: { [key: string]: string }) {
+  let fileContent = fse.pathExistsSync(envVariables.TECFANCY_CLI_DOTENV_PATH)
+    ? fse.readFileSync(envVariables.TECFANCY_CLI_DOTENV_PATH, "utf8")
+    : "";
+  let isFileChanged = false;
+  const originalLines = fileContent.split('\n').filter(line => line.trim() !== ''); // remove empty lines
+  const updatedLines = [...originalLines];
+
+  for (const [key, value] of Object.entries(envVariables)) {
+    if (key === "TECFANCY_CLI_DOTENV_PATH") continue; // skip this variable
+    let hasKey = false;
+    let isValueChanged = false;
+
+    for (let i = 0; i < originalLines.length; i++) {
+      const line = originalLines[i];
+      if (line.trim().startsWith(key + "=")) {
+        const currentValue = line.substring(key.length + 1).trim();
+        if (currentValue !== value) {
+          isValueChanged = true;
+          updatedLines[i] = `${key}=${value}`;
+          isFileChanged = true;
+        }
+        hasKey = true;
+        break;
+      }
+    }
+
+    if (!hasKey) {
+      updatedLines.push(`${key}=${value}`);
+      isFileChanged = true;
+    } else if (isValueChanged) {
+      isFileChanged = true;
+    }
+  }
+
+  if (isFileChanged) {
+    fse.writeFileSync(envVariables.TECFANCY_CLI_DOTENV_PATH, updatedLines.join("\n"));
+  }
+}
 
 /**
  * Set the default configuration
  */
 export default function setupEnvConfig() {
-  fse.appendFileSync(DOTENV_PATH, `TECFANCY_CLI_HOME=${DEFAULT_CLI_HOME}\n`); // Set the default configuration
+  updateOrCreateEnvVariables(envVariables);
 }
