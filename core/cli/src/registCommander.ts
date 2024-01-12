cf0bc79b7b1dc793d464a447f5c56654f2c71554
smpower <rf.wangchn@foxmail.com>
2024-01-12 23:28:15 +0800

    Refactor program initialization and add environment variable setting

--- core/cli/src/registCommander.ts
+++ core/cli/src/registCommander.ts
@@ -6,18 +6,43 @@ import log from "@tecfancy/log";
 
 import pkg from "../package.json";
 
-export default function registCommander() {
+/**
+ * Set environment variables
+ */
+function setEnvVariablesFromCommand(command: typeof program, flag: string, variables: Record<string, string>) {
+  command.options.forEach((option) => {
+    if (option.flags.includes(flag)) {
+      Object.keys(variables).forEach((key) => {
+        process.env[key] = variables[key];
+      });
+    }
+  });
+}
+
+function basicProgram() {
   program
     .version(pkg.version, "-v, --version", "output the current version")
     .name(Object.keys(pkg.bin)[0])
     .alias(Object.keys(pkg.bin)[1])
     .usage("<command> [options]");
+}
 
-  program
+function initProgram() {
+  const initCommand = program
     .command("init [projectName]")
     .description("init project")
     .option("-f, --force", "overwrite target directory if it exist")
+    .option("-c, --china", "use the mirror of China")
     .action(init);
+
+  setEnvVariablesFromCommand(initCommand, "-c", {
+    TECFANCY_CLI_REGISTRY_URL: "https://registry.npm.taobao.org",
+  });
+}
+
+export default function registCommander() {
+  basicProgram();
+  initProgram();
 
   program.on("command:*", function (args) {
     const availableCommands = program.commands.map((cmd) => cmd.name());
