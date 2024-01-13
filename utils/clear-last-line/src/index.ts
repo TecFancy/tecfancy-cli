9b036d9d4cd1374acb1503744e81a7b4b0e0f4e8
smpower <rf.wangchn@foxmail.com>
2024-01-13 23:47:36 +0800

    Refactor clearLastLine function to accept a parameter for the number of lines to clear

--- utils/clear-last-line/src/index.ts
+++ utils/clear-last-line/src/index.ts
@@ -2,8 +2,11 @@
 
 /**
  * Clear the last line in the terminal
+ * @param numLines number of lines to clear
  */
-export default function () {
-  process.stdout.write('\x1b[1A'); // move cursor up one line
-  process.stdout.write('\x1b[K'); // clear line
+export default function (numLines: number = 1) {
+  for (let i = 0; i < numLines; i++) {
+    process.stdout.write('\x1b[1A'); // move cursor up one line
+    process.stdout.write('\x1b[K'); // clear line
+  }
 }
