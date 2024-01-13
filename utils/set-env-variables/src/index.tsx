43556c044d70bbb1e0a39abfd1abfd28fce9774b
smpower <rf.wangchn@foxmail.com>
2024-01-13 17:25:52 +0800

    Refactor setEnvVariables function and update setEnvVariablesFromCommand to use it

--- utils/set-env-variables/src/index.tsx
+++ utils/set-env-variables/src/index.tsx
@@ -2,14 +2,18 @@
 
 import { program } from 'commander';
 
+function setEnvVariables(key: string, value: string) {
+  process.env[key] = value;
+}
+
 /**
- * Set environment variables
+ * Set environment variables from command line arguments
  */
 export function setEnvVariablesFromCommand(command: typeof program, flag: string, variables: Record<string, string>) {
   command.options.forEach((option) => {
     if (option.flags.includes(flag)) {
       Object.keys(variables).forEach((key) => {
-        process.env[key] = variables[key];
+        setEnvVariables(key, variables[key]);
       });
     }
   });
